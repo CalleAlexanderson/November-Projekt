ea2f028d030d12f73a93ff216e61688ec987f6a0
Calle <calle.alexanderson@elev.ga.ntig.se>
2020-11-09 09:40:57 +0100

    Initial commit

---
+++ November Projekt/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Raylib_cs;
+
+namespace November_Projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(1000, 600, "IDFK");
+
+            Rectangle player = new Rectangle(200, 200, 50, 50);
+            int jump = 0;
+
+            while (!Raylib.WindowShouldClose())
+            {
+                if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))
+                {
+                    Raylib.ToggleFullscreen();
+                }
+
+                player.y += 0.2f * jump;
+
+                if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
+                {
+                    player.y -= 60;
+
+                    if (!Raylib.CheckCollisionRecs(player, floor))
+                    {
+
+                    }
+                }
+
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.BLACK);
+
+                Raylib.DrawRectangleRec(player, Color.WHITE);
+                Raylib.EndDrawing();
+
+
+            }
+        }
+    }
+}
