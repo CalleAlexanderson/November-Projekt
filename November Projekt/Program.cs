06f87177844d085352bcda515392084c23293383
Calle <calle.alexanderson@elev.ga.ntig.se>
2020-11-16 10:09:38 +0100

    Working on gravity

--- November Projekt/Program.cs
+++ November Projekt/Program.cs
@@ -8,32 +8,44 @@ namespace November_Projekt
         static void Main(string[] args)
         {
             Raylib.InitWindow(1000, 600, "IDFK");
+            Raylib.SetTargetFPS(60);
 
             Rectangle player = new Rectangle(200, 200, 50, 50);
-            int jump = 0;
+            float gravity = 1f;
+            bool touchingFLoor = false;
+            Rectangle floor = new Rectangle(0, 500, 1000, 100);
 
             while (!Raylib.WindowShouldClose())
             {
-                if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))
+
+                if (Raylib.CheckCollisionRecs(player, floor))
                 {
-                    Raylib.ToggleFullscreen();
+                    touchingFLoor = true;
+                }
+                else
+                {
+                    touchingFLoor = false;
                 }
 
-                player.y += 0.2f * jump;
-
-                if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
+                player.y += gravity;
+                if (!touchingFLoor)
                 {
-                    player.y -= 60;
+                    gravity = gravity * 1.06f;
+                }
+                else
+                {
+                    gravity = 0f;
+                }
 
-                    if (!Raylib.CheckCollisionRecs(player, floor))
-                    {
-
-                    }
+                if (touchingFLoor && Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
+                {
+                    gravity = -1;
                 }
 
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.BLACK);
 
+                Raylib.DrawRectangleRec(floor, Color.WHITE);
                 Raylib.DrawRectangleRec(player, Color.WHITE);
                 Raylib.EndDrawing();
 
